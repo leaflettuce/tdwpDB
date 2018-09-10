d315bb6e90f3bde296e41c3919803ab0486d2b22
Andrew Trick <andyjtrick@gmail.com>
2018-09-10 08:17:51 -0500

    daily cleaning of past nsnm and current.

--- src/data/clean_daily-NSNM.py
+++ src/data/clean_daily-NSNM.py
@@ -11,19 +11,23 @@ import os
 
 
 # Create empty dataframe with cols we want
-columns = ['date', 'venue', 'city', 'state', 'country', 'nsnm black tee', 'steelers tee',
+columns = ['date', 'venue', 'location', 'nsnm black tee', 'steelers tee',
            'plagues tee', 'long sleeve', 'snake hoodie', 'sound and fury tee', 'nsnm windbreaker',
            'lovers ladies muscle tank', 'enamel pin', 'script logo hat camo', 
-           'script logo hat black', 'chicago tee', 'texas tee', 'california tee']
+           'script logo hat black', 'chicago tee', 'texas tee', 'california tee', 'day_total']
 df = pd.DataFrame(columns=columns)
 
+
 data_dir = '../../data/raw/daily/nsnm/'
+
 # iterate over all files in dir and run
+i = 0
 for filename in os.listdir(data_dir):
     data_file = data_dir + filename
     # Grab sheet in excel file
     workbook = xlrd.open_workbook(data_file)
     sheet = workbook.sheet_by_index(0)
+    i += 1
     
     #initialize vars for data storage
     nsnm_black = 0
@@ -41,7 +45,7 @@ for filename in os.listdir(data_dir):
     texas = 0
     cali = 0
     
-     # Iterate over for data
+     # Iterate over for data and get info
     for row in range(sheet.nrows):
         cols = sheet.row_values(row)
         if row == 1:
@@ -49,38 +53,144 @@ for filename in os.listdir(data_dir):
         if row == 2:
             venue = cols[0]
         if row == 3:
-            ''' DO MATH FOR THESE V'''
-            city = cols[0]
-            state = cols[0]
-            country = cols[0]
-            ''' DO MATH FOR THOSE ^'''
+            location = cols[0]
             
-        if row in [28, 29, 30, 31, 32]:
-            try:
-                nsnm_black += cols[2] - cols[6]
-            except TypeError:
-                pass
+        if row in range(28, 33):
+            if type(cols[3]) == float:
+                nsnm_black += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    nsnm_black += cols[2] - cols[6]
+                except TypeError:
+                    pass
         
-        if row in [37, 38, 39, 40, 41]:
-            try:
-                steelers += cols[2] - cols[6]
-            except TypeError:
-                pass
+        if row in range(37,  42):
+            if type(cols[3]) == float:
+                steelers += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    steelers += cols[2] - cols[6]
+                except TypeError:
+                    pass
         
-        if row in [46, 47, 48, 49, 50]:
-            try:
-                plagues += cols[2] - cols[6]
-            except TypeError:
-                pass
+        if row in range(46, 51):
+            if type(cols[3]) == float:
+                plagues += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    plagues += cols[2] - cols[6]
+                except TypeError:
+                    pass
             
-        if row in [55, 56, 57, 58, 59]:
-            try:
-                long_sleeve += cols[2] - cols[6]
-            except TypeError:
-                pass
+        if row in range(55, 60):
+            if type(cols[3]) == float:
+                long_sleeve += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    long_sleeve += cols[2] - cols[6]
+                except TypeError:
+                    pass
             
-        ''' DO REMAINING'''
+        if row in range(64, 69):
+            if type(cols[3]) == float:
+                snake_hoodie += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    snake_hoodie += cols[2] - cols[6]
+                except TypeError:
+                    pass
         
-    ''' WRITE ROW TO DF'''
+        if row in range(73, 78):
+            if type(cols[3]) == float:
+                sound_and_fury += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    sound_and_fury += cols[2] - cols[6]
+                except TypeError:
+                    pass
+            
+        if row in range(82, 87):
+            if type(cols[3]) == float:
+                windbreaker += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    windbreaker += cols[2] - cols[6]
+                except TypeError:
+                    pass
         
+        if row in range(91, 95):
+            if type(cols[3]) == float:
+                lovers += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    lovers += cols[2] - cols[6]
+                except TypeError:
+                    pass
+            
+        if row in range(99, 100):
+            if type(cols[3]) == float:
+                pin += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    pin += cols[2] - cols[6]
+                except TypeError:
+                    pass
+          
+        if row in range(104, 105):
+            if type(cols[3]) == float:
+                script_camo += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    script_camo += cols[2] - cols[6]
+                except TypeError:
+                    pass
+        
+        if row in range(109, 110):
+            if type(cols[3]) == float:
+                script_black += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    script_black += cols[2] - cols[6]
+                except TypeError:
+                    pass
+        
+        if row in range(114, 119):
+            if type(cols[3]) == float:
+                chicago += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    chicago += cols[2] - cols[6]
+                except TypeError:
+                    pass
+            
+        if row in range(123, 128):
+            if type(cols[3]) == float:
+                texas += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    texas += cols[2] - cols[6]
+                except TypeError:
+                    pass
+            
+        if row in range(132, 137):
+            if type(cols[3]) == float:
+                cali += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    cali += cols[2] - cols[6]
+                except TypeError:
+                    pass
+                
+    total = (nsnm_black + steelers +plagues + long_sleeve + snake_hoodie + 
+             sound_and_fury + windbreaker + lovers + pin + script_camo + 
+             script_black + chicago + texas + cali)
+    
+    '''WRITE ROW TO DF'''
+    df.loc[i] = [date, venue, location, nsnm_black, steelers, plagues, long_sleeve, 
+               snake_hoodie, sound_and_fury, windbreaker, lovers, pin, script_camo, 
+             script_black, chicago, texas, cali, total]
+    
 '''WRITE DF TO CSV'''  
+upload_dir = '../../data/interim/daily/'
+upload_file = 'nsnm-daily'
+df.to_csv(upload_dir + upload_file + '.csv')
---
+++ src/data/clean_daily-PWD.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 10 07:54:01 2018
+
+@author: andyj
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  9 16:03:37 2018
+
+@author: andyj
+"""
+
+import pandas as pd
+import xlrd
+import os
+
+
+# Create empty dataframe with cols we want
+columns = ['date', 'venue', 'location', 'one_after', 'praise_poison',
+           'reptar', 'hoodie', 'hat', 'back_pack', 'day_total']
+df = pd.DataFrame(columns=columns)
+
+
+data_dir = '../../data/raw/daily/pwd/'
+
+# iterate over all files in dir and run
+i = 0
+for filename in os.listdir(data_dir):
+    data_file = data_dir + filename
+    # Grab sheet in excel file
+    workbook = xlrd.open_workbook(data_file)
+    sheet = workbook.sheet_by_index(0)
+    i += 1
+    
+    #initialize vars for data storage
+    one_after = 0
+    praise = 0
+    reptar = 0
+    hoodie = 0
+    hat = 0
+    back_pack = 0
+    
+    
+     # Iterate over for data and get info
+    for row in range(sheet.nrows):
+        cols = sheet.row_values(row)
+        if row == 1:
+            date = cols[0]
+        if row == 2:
+            venue = cols[0]
+        if row == 3:
+            location = cols[0]
+            
+        if row in range(8, 13):
+            if type(cols[3]) == float:
+                one_after += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    one_after += cols[2] - cols[6]
+                except TypeError:
+                    pass
+        
+        if row in range(17,  22):
+            if type(cols[3]) == float:
+                praise += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    praise += cols[2] - cols[6]
+                except TypeError:
+                    pass
+        
+        if row in range(26, 31):
+            if type(cols[3]) == float:
+                reptar += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    reptar += cols[2] - cols[6]
+                except TypeError:
+                    pass
+        
+        if row in range(35, 40):
+            if type(cols[3]) == float:
+                hoodie += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    hoodie += cols[2] - cols[6]
+                except TypeError:
+                    pass
+            
+        if row in range(44, 45):
+            if type(cols[3]) == float:
+                hat += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    hat += cols[2] - cols[6]
+                except TypeError:
+                    pass
+            
+        if row in range(49, 50):
+            if type(cols[3]) == float:
+                back_pack += cols[2] + cols[3] - cols[6]
+            else:
+                try:
+                    back_pack += cols[2] - cols[6]
+                except TypeError:
+                    pass
+            
+            
+    total = (one_after + praise + reptar + hoodie + hat + back_pack)
+    
+    '''WRITE ROW TO DF'''
+    df.loc[i] = [date, venue, location, one_after, praise, reptar, hoodie, hat,
+          back_pack, total]
+    
+'''WRITE DF TO CSV'''  
+upload_dir = '../../data/interim/daily/'
+upload_file = 'pwd-daily'
+df.to_csv(upload_dir + upload_file + '.csv')
