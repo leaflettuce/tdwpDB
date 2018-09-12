0695e2da92c8c94179ad8daf3406b5252963ff1f
Andrew Trick <andyjtrick@gmail.com>
2018-09-12 13:58:09 -0500

    Update presale cleaner to set types.

--- src/data/clean_presales.py
+++ src/data/clean_presales.py
@@ -18,10 +18,32 @@ for filename in os.listdir(data_dir):
     print(filename)
     data_file = filename
     df = pd.read_csv(data_dir + data_file, skiprows = 5, encoding = "ISO-8859-1")
-     
-    df = df.drop(['On Sale', 'Wrap'], axis = 1)
+    
+    try:
+        df = df.drop(['On Sale', 'Wrap'], axis = 1)
+    except KeyError:
+        pass
+    
     if 'parkway' in filename:
         df = df.drop(df.iloc[:, 3:5], axis=1)
+    
     df = df.drop(df.iloc[:, 8:], axis=1)
     
+    #rename cols
+    df = df.rename(columns={'% Sold': 'Percent Sold'})
+    
+    # set types
+    col_names = ['Sellable', 'Percent Sold', 'Open','Total Sold', 'Days Out']
+    
+    for col_name in col_names:
+        try:
+            df[col_name] = df[col_name].str.replace(',', '').str.replace('$', '').str.replace('%', '')
+        except (AttributeError, KeyError):
+            pass
+        try:
+            df[col_name] = pd.to_numeric(df[col_name])
+        except (AttributeError, KeyError):
+            pass
+    
+    #write out
     df.to_csv(upload_dir + filename, encoding = "utf-8", index = False)
