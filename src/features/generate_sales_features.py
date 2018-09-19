a7fc85db1cd6a2d35d85723feb4dfef616184ab9
Andrew Trick <andyjtrick@gmail.com>
2018-09-19 15:43:56 -0500

    changes to sales_features.

--- src/features/generate_sales_features.py
+++ src/features/generate_sales_features.py
@@ -16,3 +16,34 @@ presale_file_name = 'ps_saleS_1.0.csv'
 
 df = pd.read_csv(data_dir + file_name)
 ps = pd.read_csv(data_dir + presale_file_name)
+
+
+# month numeric
+df['month'] = df['Date'] # CUT OFF AFTER '/'
+ps['month'] = ps['Date'] # CUT OFF AFTER '/'
+
+# regions
+df['region'] = ''
+ps['region'] = ''
+
+region_dict = {
+        'W' : [],
+        'S' : [],
+        'NW' : [],
+        'NE' : []
+        }
+
+for row in len(df):
+    for key, value in region_dict.values():
+        for state in value:
+            if state == row.iloc[3]:
+                row.iloc[22] == key
+                
+# day of week
+        ''' DO THIS '''
+        
+
+''' PRESALE FEATURES '''
+# Precent day of
+ps['percent_total_presale'] = (ps['Total Sold']/ps['Attend'])*100
+                
