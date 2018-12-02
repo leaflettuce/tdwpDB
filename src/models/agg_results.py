fc9dd4ec3ad55319fe144b5662e97add08628df9
Andrew Trick <andyjtrick@gmail.com>
2018-12-01 18:47:26 -0600

    adding presales to forecast.

--- src/models/agg_results.py
+++ src/models/agg_results.py
@@ -8,5 +8,7 @@ Created on Tue Oct 23 16:10:08 2018
 # TO DO
 
 # - IMPORT SALES and PRESALES prediction csv's
+
 # - Average out together w/ past predictions per show
+
 # - Report back final predicted
